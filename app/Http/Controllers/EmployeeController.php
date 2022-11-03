988fecaeb46ad25a319ba200560ec75ae9409c24
Sharada <boharasharada736@gmail.com>
2022-11-03 14:04:16 +0545

    push

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+   
+    public function index()
+    {
+        
+    }
+
+    
+    public function create()
+    {
+        
+    }
+
+    
+    public function store(Request $request)
+    {
+        $employee = new Employee;
+        $employee->uuid=$request->uuid;
+        $employee->name=$request->name;
+        $employee->designation=$request->designation;
+        $employee->contact=$request->contact;
+        // $employee->employee_type=$request->employee_type;
+        $employee->address=$request->address;
+        if($employee->employee_type == "p"){
+        uuid == p;
+        }
+        
+        if($request->hasfile('file')){
+
+        $file = $request->file('file');
+        $extension = $file->getClientOriginalExtension();
+        $filename = time().'.'.$extension;
+        $file->move('uploads/students/',$filename);
+        $employee->file=$filename;
+        }
+        $employee->save();
+        return redirect()->route('view-student');
+    }
+
+    
+    public function show(Employee $employee)
+    {
+        //
+    }
+
+    
+    public function edit(Employee $employee)
+    {
+        //
+    }
+
+    
+    public function update(Request $request, Employee $employee)
+    {
+        //
+    }
+
+    
+    public function destroy(Employee $employee)
+    {
+        //
+    }
+}
